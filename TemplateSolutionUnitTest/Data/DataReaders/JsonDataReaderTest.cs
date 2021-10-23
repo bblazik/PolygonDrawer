5dfa3278e3ba665a1ef261730834f2275b93969f
unknown <bartek.blazik@gmail.com>
2021-10-24 00:05:05 +0200

    Add some testing, Do some clean up

---
+++ TemplateSolutionUnitTest/Data/DataReaders/JsonDataReaderTest.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TemplateSolution.Data.DataReaders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplateSolution.Data.DataReaders.Tests
+{
+    [TestClass()]
+    public class JsonDataReaderTest
+    {
+        [TestMethod()]
+        public void WhenReadCorrectInputDataThenDataNotNull()
+        {
+            var x = new JsonDataReader("..\\..\\Input.json").Read();
+            Assert.IsNotNull(x);
+        }
+
+        [TestMethod()]
+        public void WhenReadIncorrectDataThenThrowError()
+        {
+            Assert.ThrowsException<FormatException>(() => new JsonDataReader("..\\..\\InputErrors.json").Read());
+        }
+    }
+}
---
+++ TemplateSolutionUnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TemplateSolutionUnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TemplateSolutionUnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e43ed6c3-e4da-4ff9-b7f6-45ce0a1a3671")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
